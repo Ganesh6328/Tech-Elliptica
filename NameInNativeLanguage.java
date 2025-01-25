f2c26ebccec485f3bb55d4db0b827627a06d7742
ganeshshinde5492@gmail.com <G@nesh3030>
2025-01-25 12:26:36 +0530

    Java program - Your name in your native language

---
+++ NameInNativeLanguage.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class NameInNativeLanguage {
+    public static void main(String[] args) {
+        char ch1 = '\u0917';
+        char ch2 = '\u0923';
+        char ch3 = '\u0947';
+        char ch4 = '\u0936';
+
+        System.out.println(ch1 + "" +ch2 + "" +ch3 + "" +ch4);
+
+    }
+}
